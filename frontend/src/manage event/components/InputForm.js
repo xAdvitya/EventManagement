93da9e8e69cbfb09d4e2ae0209273ccc1c2c77b6
bikaraj <bikarajsingha@gmail.com>
2021-03-07 15:45:00 +0530

    event page complete design

--- frontend/src/manage event/components/InputForm.js
+++ frontend/src/manage event/components/InputForm.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import './InputForm.css'
 
 const InputForm = () => {
     return (
-        <div>
+        <div class="plan_event">
             <form>
-                <label>Event name</label>
-                <input name="title" type="text" />
-                  <label>Description</label>
-                  <textarea name="description"></textarea>
-                  <input name="images" type="file" multiple/>
-                  <input name="date" type="date" />
+                <label for="1">Event Name</label>
+                <input name="title" type="text" id="1"/>
+                  <label for="2">Event Description</label>
+                  <textarea name="description" id="2"></textarea>
+                  <label for="3">Add Pictures</label>
+                  <input name="images" type="file" multiple id="3"/>
+                  <label for="last_date">Event Time</label>
+                  <input name="date" type="date" id="last_date"/>
                   <button type="submit">
                       submit
                   </button>
