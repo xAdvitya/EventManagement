cd1a8e2542f19e779e0771d69322888ab27bc8b1
bikaraj <bikarajsingha@gmail.com>
2021-03-07 13:23:22 +0530

    sign up page done

--- frontend/src/shared/components/login/pages/Login.js
+++ frontend/src/shared/components/login/pages/Login.js
@@ -1,18 +1,17 @@
 import React from 'react'
+import './Login.css'
 
 const Login = (props) => {
     return (
         <div>
-            <h1>LOGO</h1>
-            <form>
-                <label>username</label>
-                <input type="text" name="username"/>
-                <label>email</label>
-                <input type="email" name="email"/>
-                <label>password</label>
-                <input type="password" name="password"/>
-                <label>retype password</label>
-                <input type="password" name="password"/>
+            <h1>EVENT MANAGEMENT</h1>
+            <form class="login">
+                <h2>Register</h2>
+                <input type="text" name="username" placeholder="Username"/>
+                <input type="email" name="email"  placeholder="Email"/>
+                <input type="password" name="password"  placeholder="Password"/>
+                <input type="password" name="password"  placeholder="Confirm Password"/>
+                <input type="submit" value="Register Now" />
             </form>
         </div>
     )
